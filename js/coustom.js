b016b38b8c39b97aa16153278a07c8a2e89e6de8
Utter Pompously <54849664+utterpompously@users.noreply.github.com>
2020-02-24 19:05:40 +0600

    header and slider create

--- js/coustom.js
+++ js/coustom.js
@@ -1,4 +1,31 @@
+$(window).on('load', function() {
+	$('.sf-menu').superfish();
 
-
+   
   
-
+}); 
+ $('.slide-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsiveClass: true,
+        
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                loop: true
+            },
+            600: {
+                    items: 1,
+                    dots: true,
+                    nav: false
+                },
+                1000: {
+                    items: 1,
+                    dots: true,
+                    nav: false,
+                    loop: true
+                }
+        }
+    });
